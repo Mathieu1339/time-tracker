0f50b862ebcedc00392341b20f3354746a5e7e58
Mathieu <samkoonmatthieu@gmail.com>
2024-11-28 14:54:18 +0100

    fixed TrackerCoreConfig

--- core/src/main/java/training/taylor/timetracker/core/TrackerCoreConfig.java
+++ core/src/main/java/training/taylor/timetracker/core/TrackerCoreConfig.java
@@ -17,7 +17,7 @@ import training.taylor.timetracker.core.dao.TimeEntry;
 public class TrackerCoreConfig {
 
     @Bean(name = "timesheet")
-    public List<TimeEntry> timeEntries() {
+    List<TimeEntry> timeEntries() {
         return new ArrayList<>();
     }
 }
